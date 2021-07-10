1b0a2fb1c3be4e7a77b19826c78b6a0fdef34d03
tmiu <miu.teo@gmail.com>
2021-07-10 09:45:34 +0300

    daily logs

--- src/main/webapp/app/entities/food/food.model.ts
+++ src/main/webapp/app/entities/food/food.model.ts
@@ -5,7 +5,7 @@ import { IFoodNutritionalValue } from 'app/entities/food-nutritional-value/food-
 export interface IFood {
   id?: number;
   di?: dayjs.Dayjs | null;
-  quantity?: number | null;
+  quantity?: number | 0;
   meal?: IMeal | null;
   foodNutritionalValue?: IFoodNutritionalValue;
 }
@@ -14,7 +14,7 @@ export class Food implements IFood {
   constructor(
     public id?: number,
     public di?: dayjs.Dayjs | null,
-    public quantity?: number | null,
+    public quantity?: number | 0,
     public meal?: IMeal | null,
     public foodNutritionalValue?: IFoodNutritionalValue
   ) {}
--- src/main/webapp/app/home/daily-logs/daily-logs.component.ts
+++ src/main/webapp/app/home/daily-logs/daily-logs.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { UserStatusService } from '../../entities/user-status/service/user-status.service';
 import { IUserStatus } from '../../entities/user-status/user-status.model';
 import { HttpResponse } from '@angular/common/http';
+import { FoodService } from '../../entities/food/service/food.service';
+import { IFood } from '../../entities/food/food.model';
 
 @Component({
   selector: 'jhi-daily-logs',
@@ -10,12 +12,25 @@ import { HttpResponse } from '@angular/common/http';
 })
 export class DailyLogsComponent implements OnInit {
   userStatuses?: IUserStatus[];
+  foods?: IFood[];
 
-  constructor(private userStatusService: UserStatusService) {}
+  sumOfProtein = 0;
+  sumOfFat = 0;
+  sumOfCarbo = 0;
+
+  constructor(private userStatusService: UserStatusService, private foodService: FoodService) {}
 
   ngOnInit(): void {
     this.userStatusService.query().subscribe((res: HttpResponse<IUserStatus[]>) => {
       this.userStatuses = res.body ?? [];
+    });
+    this.foodService.query().subscribe(response => {
+      this.foods = response.body ?? [];
+      this.foods.forEach(food => {
+        this.sumOfProtein += (food.quantity! / food.foodNutritionalValue!.quantity!) * food.foodNutritionalValue!.protein!;
+        this.sumOfCarbo += (food.quantity! / food.foodNutritionalValue!.quantity!) * food.foodNutritionalValue!.carbohydrate!;
+        this.sumOfFat += (food.quantity! / food.foodNutritionalValue!.quantity!) * food.foodNutritionalValue!.fat!;
+      });
     });
   }
 }
